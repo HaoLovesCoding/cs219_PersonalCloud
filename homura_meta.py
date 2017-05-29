c26a8abb1a1ba2e58917d9199170d3ec21acb1f5
HaoLovesCoding <nanocmp@gmail.com>
2017-05-29 00:49:00 -0700

    Casual_consistency

--- homura_meta.py
+++ homura_meta.py
@@ -2,13 +2,15 @@ import os, os.path
 from xml.dom.minidom import *
 import hashlib
 from sets import Set
+from collections import namedtuple
+import copy
+
 class HomuraMeta:
+	DirStruct = namedtuple("DirStruct","path timestamp")
+
 	def __init__(self):
 		self.mydoc=None
 		self.clouddoc=None
-		self.moveSet=Set()#include the tuple of original path and new path
-		self.deleteSet=Set()#include the path of delete
-		self.createSet=Set()
 
 	def path2Xml(self,path):
 		try:
@@ -64,9 +66,66 @@ class HomuraMeta:
 		except Exception:
 			print 'Loading Warning: File or directory not exists'
 
+	#pass the DomNode into the function and get the intersection by the standard
+	def intersectByWeakStd(self,children_list1,children_list2):
+		set1_dir=Set()
+		set1_file=Set()
+		set2_file=Set()
+		set2_dir=Set()
+		FileStruct = namedtuple("FileStruct", "name md5")
+		result1=[]
+		result2=[]
+		for x in children_list1:
+			if x.nodeName=='dir':
+				set1_dir.add(x.getAttribute('path'))
+			if x.nodeName=='file':
+				set1_file.add(FileStruct(x.getAttribute('name'), x.getAttribute('md5')))
+		for x in children_list2:
+			if x.nodeName=='dir':
+				set2_dir.add(x.getAttribute('path'))
+			if x.nodeName=='file':
+				set2_file.add(FileStruct(x.getAttribute('name'), x.getAttribute('md5')))
+		for x in children_list1:
+			if x.nodeName=='dir' and x.getAttribute('path') in set2_dir:
+				result1.append(x)
+			if x.nodeName=='file' and FileStruct(x.getAttribute('name'), x.getAttribute('md5')) in set2_file:
+				result1.append(x)
+		for x in children_list2:
+			if x.nodeName=='dir' and x.getAttribute('path') in set1_dir:
+				result2.append(x)
+			if x.nodeName=='file' and FileStruct(x.getAttribute('name'), x.getAttribute('md5')) in set1_file:
+				result2.append(x)
+		return (result1,result2)
+
+	def casualConsistentCompare(self):
+		my_q=[self.mydoc]
+		cloud_q=[self.clouddoc]
+		while my_q and cloud_q:
+			my_cur=my_q.pop(0)
+			cloud_cur=cloud_q.pop(0)
+			if my_cur.nodeName=='file':
+				print my_cur.getAttribute('name')
+			if cloud_cur.nodeName=='dir':
+				print cloud_cur.getAttribute('path')
+			intersect1,intersect2=self.intersectByWeakStd(my_cur.childNodes,cloud_cur.childNodes)
+			my_q+=intersect1
+			cloud_q+=intersect2
+
+	def BFS(self):
+		q=[self.mydoc]
+		while q:
+			cur=q.pop(0)
+			obj=DomChildrenList(cur.childNodes)
+			q+=cur.childNodes
+			print cur.childNodes
+			obj.show()
+
 	def weakConsistentCompare(self):
 		myfile_dict={}
 		cloudfile_dict={}
+		moveSet=Set()#include the tuple of original path and new path
+		deleteSet=Set()#include the path of delete
+		createSet=Set()
 		if self.mydoc:
 			for my_file in self.mydoc.getElementsByTagName('file'):
 				my_file_md5=my_file.getAttribute('md5')
@@ -83,18 +142,18 @@ class HomuraMeta:
 			if  my_md5 in cloudfile_dict and myfile_dict[my_md5]==cloudfile_dict[my_md5]:
 				continue
 			if  my_md5 in cloudfile_dict and myfile_dict[my_md5]!=cloudfile_dict[my_md5]:
-				self.moveSet.add((myfile_dict[my_md5],cloudfile_dict[my_md5]))
+				moveSet.add((myfile_dict[my_md5],cloudfile_dict[my_md5]))
 				continue
 			if  my_md5 not in cloudfile_dict:
-				self.deleteSet.add(myfile_dict[my_md5])
+				deleteSet.add(myfile_dict[my_md5])
 				continue
 		for cloud_md5 in cloudfile_dict:
 			if cloud_md5 not in myfile_dict:
-				self.createSet.add(cloudfile_dict[cloud_md5])
+				createSet.add(cloudfile_dict[cloud_md5])
 		#print myfile_dict
 		#print cloudfile_dict
 
-	def showOp(self):
+	def weakConsistentshowOp(self):
 		for x in self.moveSet:
 			print 'move: '+x[0]+'=>'+x[1]
 		for x in self.deleteSet:
@@ -104,14 +163,15 @@ class HomuraMeta:
 
 if __name__ == "__main__":
 	my_meta=HomuraMeta()
-	my_meta.path2Xml('/Users/HaoWu/Documents/Code/CS219/Syncronizer/my_file')
+	my_meta.path2Xml('/Users/HaoWu/Documents/Code/CS219/Syncronizer/A')
 	my_meta.showMyXml()
 	
 	cloud_meta=HomuraMeta()
-	cloud_meta.path2Xml('/Users/HaoWu/Documents/Code/CS219/Syncronizer/cloud_file')
+	cloud_meta.path2Xml('/Users/HaoWu/Documents/Code/CS219/Syncronizer/B')
 	cloud_meta.saveXml('test.xml')
 
 	my_meta.loadXml('test.xml')
 	my_meta.showCloudXml()
-	my_meta.weakConsistentCompare()
-	my_meta.showOp()
+	#my_meta.weakConsistentCompare()
+	#my_meta.BFS()
+	my_meta.casualConsistentCompare()
