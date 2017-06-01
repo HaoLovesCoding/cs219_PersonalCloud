45b6861ba17411d20bc289f6bd18394b90ae0804
william1lai <williamlai@ucla.edu>
2017-06-01 14:00:33 -0700

    implemented basic functions; placeholder in place until XML API is ready

--- homura_hdfs.py
+++ homura_hdfs.py
@@ -3,123 +3,84 @@ from hdfs import Config
 import time
 import os
 
+def log(message, error=0):
+    if error == 0:
+        print 'Log:', message
+    else:
+        print 'Error:', message
+
 
 class HomuraFS():
 
     def __init__(self, name):
         self.client = Config().get_client('dev')
-        self.uploaded_files = 'uploaded_files.txt'
-        self.request_file = 'hadooptest/request_log.txt'
         self.prompt = 'homura_fs $ '
         self.name = name
+        self.local_xml = 'madoka.xml'
+        self.hdfs_xml = 'name/madoka.xml'
 
     def shell_loop(self):
         while True:
-            cmd = raw_input(self.prompt).split(' ')
+            cmd = raw_input(self.prompt)
 
-            if len(cmd) == 1 and (cmd[0] == 'quit' or cmd[0] == 'unmount'):
-                self.handle_unmount()
-                break
-            elif len(cmd) == 1 and cmd[0] == 'help':
-                print 'placeholder for help'
-            elif len(cmd) == 1 and cmd[0] == 'ls':
-                print 'placeholder for ls'
-            elif cmd[0] == 'read':
-                filepath = cmd[1]
-                self.handle_read(filepath)
-            elif cmd[0] == 'upload':
-                filepath = cmd[1]
-                hadoop_path = cmd[2]
-                self.handle_upload(filepath, hadoop_path, 0) # don't copy actual file
+            if cmd == 'sync':
+                log('Syncing files')
+                self.sync_files()
+            elif cmd == 'quit':
+                return
 
-            self.check_request_log()
+    def sync_files(self):
+        # fetch XML file of last sync
+        # generate current XML file of device
+        # find operations between last sync and current device
+        # fetch current XML file of HDFS
+        # find operations between last sync and current HDFS
+        # apply operations on current device
+        # apply operations on HDFS
+        # update last sync for both HDFS and current device
+        pass
 
-    def check_request_log(self):
-        print 'Log: Checking request log'
-        with self.client.read(self.request_file) as reader:
-            lines = reader.read().split('\n')
-            buf = ''
-            for line in lines:
-                if line == '':
-                    continue
-                host, origin_path, hadoop_path = line.split(',')
-                if host == self.name:
-                    self.handle_upload(origin_path, hadoop_path, 1)
-                else:
-                    buf += line + '\n'
-            self.client.write(self.request_file, overwrite=True, data=buf, append=False)
+    # in this set of functions, when kyuubey = 0, the operation goes
+    # from loc to hdfs (i.e. local becomes the "master")
+    # when kyuubey = 1, the operation goes from hdfs to loc
+    # (i.e. hdfs becomes the "master")
+    def create_file(self, loc_path, hdfs_path, kyuubey):
+        if kyuubey == 0:
+            log('Creating file ' + hdfs_path + ' on HDFS')
+            self.client.upload(hdfs_path, loc_path, n_threads=0)
+        elif kyuubey == 1:
+            log('Creating file ' + loc_path + ' locally')
+            self.client.download(hdfs_path, loc_path, n_threads=0)
 
-    def handle_unmount(self):
-        print 'Log: Attempting to unmount device'
-        with open(self.uploaded_files, 'r+') as upfile:
-            print 'Log: Checking uploads file'
-            for line in [line.rstrip('\n') for line in upfile]:
-                filepath, hadoop_path = line.split(',')
-                with self.client.read(hadoop_path) as hfile:
-                    with open(filepath, 'w') as writer:
-                        print 'Log: Writing ' + filepath + ' back to disk'
-                        writer.write(hfile.read())
-                        print 'Log: Deleting ' + hadoop_path + ' from HDFS'
-                        self.client.delete(hadoop_path)
-        os.remove(self.uploaded_files)
-
-    def handle_upload(self, filepath, hadoop_path, upload_actual):
-        with open(filepath) as reader:
-            with self.client.write(hadoop_path, overwrite=True) as writer:
-                if upload_actual:
-                    print 'Log: Uploading actual file'
+    def update_file(self, loc_path, hdfs_path, kyuubey):
+        if kyuubey == 0: # updating file on HDFS
+            log('Updating file ' + hdfs_path + ' on HDFS')
+            with open(loc_path) as reader:
+                with self.client.write(hdfs_path, overwrite=True) as writer:
                     for line in reader:
                         writer.write(line)
-                    with open(self.uploaded_files, "a+") as upfile:
-                        upfile.write(filepath + ',' + hadoop_path + '\n')
-                        print 'Log: Adding file to uploads file'
-                else:
-                    print 'Log: Uploading metadata'
-                    writer.write('METADATA\n')
-                    writer.write(self.name + '\n')
-                    writer.write(filepath + '\n')
-
-    def handle_read(self, filepath):
-        while True:
-            exists = self.exists_file(filepath)
-            if exists == 'DNE':
-                print 'Error: Could not find file with name ' + filepath
-                return
-            elif exists == 'METADATA':
-                print 'Log: Only metadata exists right now'
-                with self.client.read(filepath) as reader:
+        elif kyuubey == 1:
+            log('Updating file ' + loc_path + ' locally')
+            with open(loc_path) as writer:
+                with self.client.read(hdfs_path) as reader:
                     data = reader.read()
-                    lines = data.split('\n')
-                    host = lines[1]
-                    origin_path = lines[2]
-                    req = host + ',' + origin_path + ',' + filepath + '\n'
-                    exists_req = False
-                    with self.client.read(self.request_file) as reader2:
-                        lines = reader2.read().split('\n')
-                        for line in lines:
-                            if req == line + '\n': # request already in
-                                print 'Log: Request exists already'
-                                exists_req = True
-                                break
-                    if not exists_req:
-                        self.client.write(self.request_file, data=req, append=True)
-                        print 'Log: Request entered into log'
+                    writer.write(data)
 
-            else:
-                print exists # contains data otherwise
-                return
-
-    def exists_file(self, filepath):
-        try:
-            with self.client.read(filepath) as reader:
-                data = reader.read()
-                lines = data.split('\n')
-                if lines[0] == 'METADATA':
-                    return 'METADATA' # only metadata
-                else:
-                    return data # data exists
-        except:
-            return 'DNE' # file does not exist
+    def delete_file(self, path, kyuubey):
+        if kyuubey == 0: # delete file on HDFS
+            log('Deleting file ' + path + ' from HDFS')
+            self.client.delete(path, recursive=True)
+        elif kyuubey == 1: # delete file locally
+            log('Deleting file ' + path + ' locally')
+            os.remove(path)
+            
+    def move_file(self, src_path, dst_path, kyuubey):
+        if kyuubey == 0: # move file on HDFS
+            log('Moving file from ' + src_path + ' to ' + dst_path + ' on HDFS')
+            self.client.rename(src_path, dst_path)
+        elif kyuubey == 1: # move file locally
+            os.rename(src_path, dst_path)
+            log('Moving file from ' + src_path + ' to ' + dst_path + ' locally')
 
 
 if __name__ == "__main__":
