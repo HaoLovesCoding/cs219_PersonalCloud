a00544cd50f47eb7d9f03262b31110877b355d58
yanxiao0201 <yanxiao0201@gmail.com>
2017-06-03 11:47:48 -0700

    add conflict support for both

--- ConflictResolver.py
+++ ConflictResolver.py
@@ -38,22 +38,21 @@ class ConflictResolver(object):
                 idx = modify_b.index(entry)
                 del modify_b[idx]
 
-            elif result == "Both":
+            elif (result == "Both") or (result == "both"):
                 (local_dirpath,old_filename) = self.find_local_dir(entry)
                 if old_filename not in os.listdir(local_dirpath):
                     raise NameError, old_filename + " is not on the local computer"
                 else:
-                    old_fullname = self.recFindName()
-                    #old_fullname = local_dirpath + os.path.sep + old_filename
-                    new_fullname = old_fullname + "_" + host_a
+                    old_fullname = local_dirpath + os.path.sep + old_filename
+                    new_fullname, new_filename = self.recFindname(old_filename, host_a, local_dirpath)
                     os.rename(old_fullname, new_fullname)
 
                     idx = modify_b.index(entry)
                     del modify_b[idx]
 
                     #create a new file named new name
-                    new_name = entry + "_" + host_a
-                    create_b.append(new_name)
+                    new_entry = self.generate_entry(entry,new_filename)
+                    create_b.append(new_entry)
 
         # solve creation conflict
 
@@ -68,6 +67,24 @@ class ConflictResolver(object):
                 elif result == host_b:# leave host_b copy
                     idx = create_b.index(entry)
                     del create_b[idx]
+
+                elif (result == "Both") or (result == "both"):
+                    (local_dirpath,old_filename) = self.find_local_dir(entry)
+                    if old_filename not in os.listdir(local_dirpath):
+                        raise NameError, old_filename + " is not on the local computer"
+                    else:
+                        old_fullname = local_dirpath + os.path.sep + old_filename
+                        new_fullname, new_filename = self.recFindname(old_filename, host_a, local_dirpath)
+                        os.rename(old_fullname, new_fullname)
+
+                        idx = create_b.index(entry)
+                        del create_b[idx]
+
+                        #create a new file named new name
+                        new_entry = self.generate_entry(entry,new_filename)
+                        create_b.append(new_entry)
+
+
             else:# a folder
                 idx1 = create_a.index(entry)
                 del create_a[idx1]
@@ -110,7 +127,7 @@ class ConflictResolver(object):
 
 
     def user_input(self,entry,host_a,host_b,type_a,type_b):
-
+        print ""
         print "There is conflict on a file: " + entry
         print host_a + " wants to " + type_a
         print host_b + " wants to " + type_b
@@ -118,11 +135,11 @@ class ConflictResolver(object):
         print "usage: input "+ host_a + " or " + host_b
 
         while True:
-             result = raw_input()
+             result = raw_input("Answer:")
              if result == host_a or result == host_b:
                  return result
             #TODO: Add support for keeping both copy of the files
-             elif result == "Both" and type_a == type_b:
+             elif ((result == "Both") or (result == "both")) and type_a == type_b:
                  return result
              else:
                  print "Wrong input"
@@ -146,5 +163,19 @@ class ConflictResolver(object):
             filename = local_filepath[i+1:]
             return (local_dirpath,filename)
 
-    def recFindname(self):
-        return
+    def recFindname(self,old_filename, host_a, local_dirpath):
+        new_filename = old_filename
+        while True:
+            new_filename += "_" + host_a
+            if new_filename not in os.listdir(local_dirpath):
+                break
+        new_fullname = local_dirpath + new_filename
+        return new_fullname, new_filename
+
+    def generate_entry(self,entry,new_filename):
+        for i in xrange(len(entry)-1, -1, -1):
+            if entry[i] == "/":
+                break
+        dir_path = entry[:i]
+        new_entry = dir_path + os.path.sep + new_filename
+        return new_entry
