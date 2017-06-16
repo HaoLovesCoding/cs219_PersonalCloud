46dc204bfa64f18b19caa255251d1cd6ed3176b1
yanxiao0201 <yanxiao0201@gmail.com>
2017-06-15 17:18:50 -0700

    fix

--- reset.sh
+++ reset.sh
@@ -3,19 +3,14 @@
 rm /Volumes/USB1/.cur_hdfs.xml
 rm /Volumes/USB1/.last_sync.xml
 
-hadoop fs -rm -r /cs219
 hadoop fs -rm /.cur_hdfs.xml
 hadoop fs -rm /.last_sync.xml
-hadoop fs -rm -r /class
+hadoop fs -rm -r /cs219
 
 rm /Volumes/USB2/.cur_hdfs.xml
 rm /Volumes/USB2/.last_sync.xml
 
 
-rm -r /Volumes/USB2/school
-
-rm -r /Volumes/USB2/class
 
 
-rm /Volumes/USB2/exam.txt
-rm /Volumes/USB2/homework.txt
+
